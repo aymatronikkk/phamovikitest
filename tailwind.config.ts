87592c1927f3271d8c030111cf0fce65051f72c4
aymatronikkk <134143387+aymatronikkk@users.noreply.github.com>
2025-01-19 21:10:34 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,60 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "#1A1F2C",
+        foreground: "#F6F6F7",
+        primary: {
+          DEFAULT: "#9b87f5",
+          foreground: "#F6F6F7",
+        },
+        secondary: {
+          DEFAULT: "#7E69AB",
+          foreground: "#F6F6F7",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "#2A2F3C",
+          foreground: "#9F9EA1",
+        },
+        accent: {
+          DEFAULT: "#6E59A5",
+          foreground: "#F6F6F7",
+        },
+        card: {
+          DEFAULT: "#2A2F3C",
+          foreground: "#F6F6F7",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
